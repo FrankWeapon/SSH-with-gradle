2245ec99854d4f8e03087ce0e8533feba76870f5
frankweapon <helldarkfire@gmail.com>
2016-10-12 11:53:16 +0800

    Modify the error in configure files and add the login demo

---
+++ src/test/java/org/fw/dao/UserDaoTest.java
@@ -0,0 +1,11 @@
+package org.fw.dao;
+
+import static org.junit.Assert.*;
+
+/**
+ * File created by FrankWeapon on 9/20/16 for ssh.
+ * Email: helldarkfire@gmail.com
+ */
+public class UserDaoTest {
+
+}
